5258e90811855bab9d5e469116070f210570956c
plutoclement <plutoclement@hotmail.fr>
2016-04-10 19:27:22 +0200

    Update and rename init_temp to init_temp.sh

--- init_temp.sh
+++ init_temp.sh
@@ -1,15 +1,15 @@
-
+#!/bin/sh
 case "$1" in 
     start)
         echo "Starting temp2table"
-        sudo /home/jeedom/temp2table/temp
+        sudo /home/jeedom/temp2table/temp&
         ;;
     stop)
         echo "Stopping temp2table"
         killall temp
         ;;
     *)
-        echo "Usage: /etc/init.d/init_temp start|stop"
+        echo "Usage: /etc/init.d/init_temp.sh start|stop"
         exit 1
         ;;
 esac
